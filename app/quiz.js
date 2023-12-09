a48fdf7725938098db5db71144f577ffbbc2c500
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-08 22:40:53 -0700

    fixing formatting

--- app/quiz.js
+++ app/quiz.js
@@ -14,90 +14,91 @@ const Quiz = () => {
     const [timer, setTimer] = useState(15);
 
     const handleStartQuiz = () => {
-    setQuizStarted(true);
-    };
+        setQuizStarted(true);
+        };
 
     const handleAnswerOptionClick = useCallback((isCorrect) => {
-    if (isCorrect) {
-        setScore(score + 1);
-    }
+        if (isCorrect) {
+            setScore(score + 1);
+        }
 
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
-        setCurrentQuestion(nextQuestion);
-        setTimer(15); 
-    } else {
-        setShowScore(true);
-    }
-    }, [currentQuestion, score]);
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+            setTimer(15); 
+        } else {
+            setShowScore(true);
+        }
+        }, [currentQuestion, score]);
 
     useEffect(() => {
-    let timerId;
+        let timerId;
 
-    if (quizStarted && !showScore) {
-        timerId = setInterval(() => {
-        setTimer((prevTimer) => {
-            if (prevTimer === 0) {
+        if (quizStarted && !showScore) {
+            timerId = setInterval(() => {
+            setTimer((prevTimer) => {
+                if (prevTimer === 0) {
+                clearInterval(timerId);
+                handleAnswerOptionClick(false);
+                return 15;
+                }
+                return prevTimer - 1;
+            });
+            }, 1000);
+        }
+
+        return () => {
             clearInterval(timerId);
-            handleAnswerOptionClick(false);
-            return 15;
-            }
-            return prevTimer - 1;
-        });
-        }, 1000);
-    }
-
-    return () => {
-        clearInterval(timerId);
-    };
+        };
     }, [handleAnswerOptionClick, quizStarted, showScore, timer]);
 
     const handleRetry = () => {
-    setCurrentQuestion(0);
-    setShowScore(false);
-    setScore(0);
-    setTimer(15);
-    };
+        setCurrentQuestion(0);
+        setShowScore(false);
+        setScore(0);
+        setTimer(15);
+        };
 
     const calculateTimerPercentage = () => {
-    return (timer / 15) * 100;
-    };
+        return (timer / 15) * 100;
+        };
 
     return (
-    <div className="app">
-        {!quizStarted ? (
-        <div className="start-section">
-            <h2>Welcome to PopQuizzy!</h2>
-            <p>Press Start to begin.</p>
-            <button className="start-button"onClick={handleStartQuiz}>Start</button>
+        <div className="app">
+            {!quizStarted ? (
+            <div className="start-section">
+                <h2>Welcome to PopQuizzy!</h2>
+                <p>Press Start to begin.</p>
+                <button className="start-button"onClick={handleStartQuiz}>Start</button>
+            </div>
+            ) : showScore ? (
+            <div className="results-section">
+                <h2>You scored {score} out of {questions.length}!</h2>
+                <p>Press Retry to play again.</p>
+                <button className="reset-button" onClick={handleRetry}>Retry</button>
+            </div>
+            ) : (
+            <>
+                <div className="timer-bar">
+                    <div className="timer-bar-fill" style={{ width: `${calculateTimerPercentage()}%` }}></div>
+                </div>
+                <div className="question-section">
+                <div className="question-count">
+                    <span>Question {currentQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className="question-text">{questions[currentQuestion].questionText}</div>
+                </div>
+                <div className="answer-options">
+                {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                    <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                    {answerOption.answerText}
+                    </button>
+                ))}
+                </div>
+            </>
+            )}
         </div>
-        ) : showScore ? (
-        <div className="results-section">
-            <p>You scored {score} out of {questions.length}!</p>
-            <button className="reset-button" onClick={handleRetry}>Retry</button>
-        </div>
-        ) : (
-        <>
-            <div className="timer-bar">
-                <div className="timer-bar-fill" style={{ width: `${calculateTimerPercentage()}%` }}></div>
-            </div>
-            <div className="question-section">
-            <div className="question-count">
-                <span>Question {currentQuestion + 1}</span>/{questions.length}
-            </div>
-            <div className="question-text">{questions[currentQuestion].questionText}</div>
-            </div>
-            <div className="answer-options">
-            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-                <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
-                {answerOption.answerText}
-                </button>
-            ))}
-            </div>
-        </>
-        )}
-    </div>
-    );
+        );
     };
 
     export default Quiz;
