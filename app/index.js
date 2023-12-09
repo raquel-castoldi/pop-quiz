ec37de44c1acdbea74bfe6c12196208f311406dd
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-08 19:03:11 -0700

    entry point done

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Page from './page.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
