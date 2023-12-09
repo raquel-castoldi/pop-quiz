f5142303fcf96cc1fe8f0c50351076ca1f396b56
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-08 21:05:46 -0700

    debugging

--- app/index.js
+++ app/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './page.js';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <Page />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
