531281d78be4b95b051b5eca30cc3788da2a58e8
raquel-castoldi <raquelcastoldi16@gmail.com>
2023-12-08 19:19:11 -0700

    quiz questions done

---
+++ app/quiz-data.js
@@ -0,0 +1,94 @@
+const QuizData = [
+  {
+    questionText: 'Which artist released the hit song "Blinding Lights"?',
+    answerOptions: [
+      { answerText: 'Drake', isCorrect: false },
+      { answerText: 'The Weeknd', isCorrect: true },
+      { answerText: 'Ariana Grande', isCorrect: false },
+      { answerText: 'Ed Sheeran', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'In the TV series "Stranger Things," what is the name of the parallel dimension?',
+    answerOptions: [
+      { answerText: 'The Abyss', isCorrect: false },
+      { answerText: 'The Void', isCorrect: false },
+      { answerText: 'The Other Side', isCorrect: true },
+      { answerText: 'The Upside Down', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Which Marvel superhero can shrink and grow in size?',
+    answerOptions: [
+      { answerText: 'Thor', isCorrect: false },
+      { answerText: 'Ant-Man', isCorrect: true },
+      { answerText: 'Captain America', isCorrect: false },
+      { answerText: 'Black Widow', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Who played the lead role in the movie "La La Land"?',
+    answerOptions: [
+      { answerText: 'Bradley Cooper', isCorrect: false },
+      { answerText: 'Emma Stone', isCorrect: true },
+      { answerText: 'Ryan Reynolds', isCorrect: false },
+      { answerText: 'Chris Hemsworth', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Which social media platform is known for its disappearing photos and videos?',
+    answerOptions: [
+      { answerText: 'Instagram', isCorrect: false },
+      { answerText: 'Facebook', isCorrect: false },
+      { answerText: 'Twitter', isCorrect: false },
+      { answerText: 'Snapchat', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'Which Disney Pixar film features characters named Woody and Buzz Lightyear?',
+    answerOptions: [
+      { answerText: 'Finding Nemo', isCorrect: false },
+      { answerText: 'Toy Story', isCorrect: true },
+      { answerText: 'Up', isCorrect: false },
+      { answerText: 'Inside Out', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Which pop sensation released the album "Sour"?',
+    answerOptions: [
+      { answerText: 'Taylor Swift', isCorrect: false },
+      { answerText: 'Adele', isCorrect: false },
+      { answerText: 'Olivia Rodrigo', isCorrect: true },
+      { answerText: 'Billie Eilish', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'In the TV series "The Office," who is the regional manager of Dunder Mifflin Scranton?',
+    answerOptions: [
+      { answerText: 'Jim Halpert', isCorrect: false },
+      { answerText: 'Dwight Schrute', isCorrect: false },
+      { answerText: 'Michael Scott', isCorrect: true },
+      { answerText: 'Pam Beesly', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'Which actor portrays the character Spider-Man in the Marvel Cinematic Universe?',
+    answerOptions: [
+      { answerText: 'Andrew Garfield', isCorrect: false },
+      { answerText: 'Tobey Maguire', isCorrect: false },
+      { answerText: 'Tom Holland', isCorrect: true },
+      { answerText: 'Tom Hardy', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'In the TV series "Friends," what is the name of Ross Geller\'s second wife?',
+    answerOptions: [
+      { answerText: 'Emily', isCorrect: true },
+      { answerText: 'Rachel', isCorrect: false },
+      { answerText: 'Janice', isCorrect: false },
+      { answerText: 'Carol', isCorrect: false },
+    ],
+  },
+];
+
+export default QuizData;
